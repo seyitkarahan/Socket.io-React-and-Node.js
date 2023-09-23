7379d465578e7b1a5a933c011ca279b57641eb91
seyitkarahan <karahanseyit570@gmail.com>
2023-09-23 23:50:05 +0300

    Socket.io React and Node.js
    I made, Socket.io React and Node.js

---
+++ socket-io/client/src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import io from "socket.io-client";
+import React, { useEffect, useState } from "react";
+
+const socket = io.connect("http://localhost:3001")
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+  const sendMessage = () => {
+    socket.emit("send_message", { message });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Message"
+        onChange={(e) => {
+          setMessage(e.target.value);
+        }} />
+      <button onClick={sendMessage}>Send Message</button>
+      <h1>Message:</h1>
+      {messageReceived}
+    </div>
+  )
+}
+
+export default App
